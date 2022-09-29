25a05a2196a80ba341f95ce8a322ff90a392e5fd
Sanjayasadaruwan <114632450+Sanjayasadaruwan@users.noreply.github.com>
2022-09-29 09:04:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -31,14 +31,14 @@ global.APIKeys = {
 
 // Other
 
-global.alivemsg ="HI I'M  ⚜️𝘋𝘈𝘙𝘒 𝘕𝘌𝘙𝘖 𝘔𝘋⚜️"
+global.alivemsg ="HI I'M  ⚜️😻 𝘋𝘈𝘙𝘒 𝙎𝘼𝙉𝘼 𝘽𝙊𝙏 😻⚜️"
                      
 global.alivepic = "https://i.ibb.co/8N6fR4X/Whats-App-Image-2022-09-17-at-21-22-46.jpg" //you logo key 
 
 
-global.ownername = " KAVEESHA " //ur owner name
-global.owner = ['94774579505','']
-global.premium = ['94774579505','94774579505']
+global.ownername = " SANJAYA " //ur owner name
+global.owner = ['94762176146','']
+global.premium = ['94762176146','94762176146']
 global.botname = '𝙳⃝𝙰𝚁𝙺 𝙽⃟𝙴𝚁𝙾 𝙼⃝𝙳'
 global.ownername = '𝙳⃝𝙰𝚁𝙺 𝙽⃟𝙴𝚁𝙾 𝙼⃝𝙳'
 global.group1 = 'https://chat.whatsapp.com/GkAmtuJ4jFE6axiNjoSiCX'
